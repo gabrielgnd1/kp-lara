96409d731d822884675dc8a82ab4f6771a3e2c69
angelineaureliaa <angelineaurelia123@gmail.com>
2025-05-21 20:01:54 +0700

    pushh

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ebe2bbab3954c44dcc86db556eece2ec4a530387',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ebe2bbab3954c44dcc86db556eece2ec4a530387',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
